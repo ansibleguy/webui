12985ddd3390bed6b9b7ff9fffa26c1988ba4d86
AnsibleGuy <guy@ansibleguy.net>
2024-01-17 21:52:04 +0100

    updated dev-version script

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -4,6 +4,5 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-#last_tag="$(git describe --exact-match --abbrev=0)"
-last_tag="0.0.1"
+last_tag="$(git describe --exact-match --abbrev=0)"
 echo "${last_tag}.dev" > "$(dirname "$0")/../VERSION"
