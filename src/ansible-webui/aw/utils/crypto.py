6efabffa9bc64392ba6f07db79b354bc3cbd1b19
AnsibleGuy <guy@ansibleguy.net>
2024-01-26 23:53:55 +0100

    crypto fixes

--- src/ansible-webui/aw/utils/crypto.py
+++ src/ansible-webui/aw/utils/crypto.py
@@ -1,45 +1,61 @@
 from base64 import b64encode, b64decode
-from hashlib import sha256
+from hashlib import sha256, scrypt
 
-from Crypto import Random
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 from aw.config.main import config
 from aw.utils.util import is_null
-from aw.utils.debug import log_warn
+from aw.utils.debug import log_warn, log
 
-__KEY = sha256(config['secret'].encode()).digest()
-
-
-def encrypt(raw: str) -> str:
-    if is_null(raw):
-        return ''
-
-    raw = _pad(raw)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(__KEY, AES.MODE_CBC, iv)
-    return b64encode(iv + cipher.encrypt(raw.encode())).decode('utf-8')
+__KEY = sha256(config['secret'].encode('utf-8')).digest()
 
 
-def decrypt(enc: str) -> str:
-    if is_null(enc):
+def encrypt(plaintext: str) -> str:
+    if is_null(plaintext):
         return ''
 
     try:
-        enc = b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(__KEY, AES.MODE_CBC, iv)
-        return _unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return _encrypt(plaintext.encode('utf-8')).decode('utf-8')
 
-    except ValueError:
-        log_warn("Unable to decrypt secret! Maybe the key 'AW_SECRET' changed?")
+    except ValueError as err:
+        log_warn("Unable to encrypt data!")
+        log(msg=f"Got error encrypting plaintext: '{err}'", level=6)
         return ''
 
 
-def _pad(s) -> str:
-    bs = AES.block_size
-    return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+def _encrypt(plaintext: bytes) -> bytes:
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(__KEY, AES.MODE_CBC, iv)
+    ciphertext = iv + cipher.encrypt(
+        plaintext=pad(
+            data_to_pad=plaintext,
+            block_size=AES.block_size,
+            style='pkcs7',
+        ),
+    )
+    return b64encode(ciphertext)
 
 
-def _unpad(s) -> bytes:
-    return s[:-ord(s[len(s)-1:])]
+def decrypt(ciphertext: str) -> str:
+    if is_null(ciphertext):
+        return ''
+
+    try:
+        return _decrypt(ciphertext.encode('utf-8')).decode('utf-8')
+
+    except ValueError as err:
+        log_warn("Unable to decrypt secret! Maybe the key 'AW_SECRET' changed?")
+        log(msg=f"Got error decrypting ciphertext: '{err}'", level=6)
+        return ''
+
+
+def _decrypt(ciphertext: bytes) -> bytes:
+    ciphertext = b64decode(ciphertext)
+    cipher = AES.new(__KEY, AES.MODE_CBC, ciphertext[:AES.block_size])
+    return unpad(
+        padded_data=cipher.decrypt(ciphertext[AES.block_size:]),
+        block_size=AES.block_size,
+        style='pkcs7',
+    )
